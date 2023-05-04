5426b85094372f6ea2e3f6a4d9d18f6f34d872b6
emja9213 <Emanuel.Jansson.9213@student.uu.se>
2023-05-04 17:07:16 +0200

    Font size slider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Sketch from 'react-p5'
 
 
-let checkbox, input, text, speed;
+let checkbox, input, text, speed, fontSize;
 
 function App() {
   const setup = (p5, canvasParentRef) => {
@@ -13,9 +13,13 @@ function App() {
     input = p5.createInput('Kinetic Typography');
     input.position(5, 30);
 
+    fontSize = p5.createSlider(8, 128, 64, 2);
+    fontSize.position(5, 80);
+    p5.createP('Font Size').position(50, 45);
+
     speed = p5.createSlider(0.1, 20, 3, 0.1);
-    speed.position(5, 80);
-    p5.createP('Speed').position(60, 45);
+    speed.position(5, 130);
+    p5.createP('Speed').position(60, 100);
 
     p5.textAlign(p5.CENTER);
     p5.angleMode(p5.DEGREES);
@@ -24,19 +28,18 @@ function App() {
   const draw = p5 => {
     p5.background(255);
 
-    p5.textSize(64); // TODO adjust sizes and such according to screen size
-    
     text = input.value();
-
+    p5.textSize(fontSize.value());
+    
     if (checkbox.checked()) {
-      for (var i = 0; i < 15; i++) {
+      for (var i = 0; i < 14; i++) {
         p5.push();
 
         p5.rotate(p5.tan(p5.frameCount + i * 5) * speed.value());
 
         p5.fill(0, 0, 0);
 
-        let spacing = 70;
+        let spacing = fontSize.value();
         p5.text(text, window.innerWidth / 2, window.innerHeight / 2 - i * spacing);
         p5.text(text, window.innerWidth / 2, window.innerHeight / 2 + i * spacing);
         
