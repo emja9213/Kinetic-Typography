68ff71a40c06d6e1d85ba630d0e6af282549f29f
emja9213 <Emanuel.Jansson.9213@student.uu.se>
2023-05-04 16:41:35 +0200

    Basic webpage with the ability to enter and display a text, toggle a simple rotation effect, adjust effect speed. Also removed strictmode from index.js as it causes the canvas to render twice.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import './App.css';
+import Sketch from 'react-p5'
+
+
+let checkbox, input, text, speed;
 
 function App() {
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
+    checkbox = p5.createCheckbox('Tan Rotation Effect', false);
+    checkbox.position(0, 0);
+
+    input = p5.createInput('Kinetic Typography');
+    input.position(5, 30);
+
+    speed = p5.createSlider(0.1, 20, 3, 0.1);
+    speed.position(5, 80);
+    p5.createP('Speed').position(60, 45);
+
+    p5.textAlign(p5.CENTER);
+    p5.angleMode(p5.DEGREES);
+  }
+
+  const draw = p5 => {
+    p5.background(255);
+
+    p5.textSize(64); // TODO adjust sizes and such according to screen size
+    
+    text = input.value();
+
+    if (checkbox.checked()) {
+      for (var i = 0; i < 15; i++) {
+        p5.push();
+
+        p5.rotate(p5.tan(p5.frameCount + i * 5) * speed.value());
+
+        p5.fill(0, 0, 0);
+
+        let spacing = 70;
+        p5.text(text, window.innerWidth / 2, window.innerHeight / 2 - i * spacing);
+        p5.text(text, window.innerWidth / 2, window.innerHeight / 2 + i * spacing);
+        
+        p5.pop();
+      }
+    } else {
+      p5.text(text, window.innerWidth / 2, window.innerHeight / 2);
+    }
+  }
+
+  const windowResized = p5 => {
+    p5.resizeCanvas(window.innerWidth, window.innerHeight);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Kinetic Typography
-        </p>
-      </header>
+      <Sketch setup={setup} draw={draw} windowResized={windowResized}/>
     </div>
   );
 }
