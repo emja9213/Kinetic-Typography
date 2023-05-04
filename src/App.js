2843b432a2b64619006ba955508abfd1c3e390ae
emja9213 <Emanuel.Jansson.9213@student.uu.se>
2023-05-04 18:49:08 +0200

    Wave function

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import './App.css';
 import Sketch from 'react-p5'
 
 
-let checkbox, input, text, speed, fontSize;
+let tanRotCheckbox, waveCheckbox, input, text, speed, fontSize, amplitude, waveLength;
 
 function App() {
   const setup = (p5, canvasParentRef) => {
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
-    checkbox = p5.createCheckbox('Tan Rotation Effect', false);
-    checkbox.position(0, 0);
+    
+    tanRotCheckbox = p5.createCheckbox('Tan Rotation Effect', false);
+    tanRotCheckbox.position(0, 0);
+
+    waveCheckbox = p5.createCheckbox('Wave Effect', true);
+    waveCheckbox.position(0, 30);
 
     input = p5.createInput('Kinetic Typography');
-    input.position(5, 30);
+    input.position(5, 60);
 
     fontSize = p5.createSlider(8, 128, 64, 2);
-    fontSize.position(5, 80);
-    p5.createP('Font Size').position(50, 45);
+    fontSize.position(5, 105);
+    p5.createP('Font Size').position(50, 70);
 
     speed = p5.createSlider(0.1, 20, 3, 0.1);
-    speed.position(5, 130);
-    p5.createP('Speed').position(60, 100);
+    speed.position(5, 150);
+    p5.createP('Speed').position(60, 115);
+
+    amplitude = p5.createSlider(1, 512, 16, 1);
+    amplitude.position(5, 195);
+    p5.createP('Amplitude').position(50, 160);
+
+    waveLength = p5.createSlider(0, 128, 16, 1);
+    waveLength.position(5, 240);
+    p5.createP('Wave Length').position(40, 205);
 
     p5.textAlign(p5.CENTER);
     p5.angleMode(p5.DEGREES);
@@ -31,11 +43,11 @@ function App() {
     text = input.value();
     p5.textSize(fontSize.value());
     
-    if (checkbox.checked()) {
+    if (tanRotCheckbox.checked()) {
       for (var i = 0; i < 14; i++) {
         p5.push();
 
-        p5.rotate(p5.tan(p5.frameCount + i * 5) * speed.value());
+        p5.rotate(p5.tan(p5.frameCount + i * amplitude.value()) * speed.value());
 
         p5.fill(0, 0, 0);
 
@@ -43,6 +55,21 @@ function App() {
         p5.text(text, window.innerWidth / 2, window.innerHeight / 2 - i * spacing);
         p5.text(text, window.innerWidth / 2, window.innerHeight / 2 + i * spacing);
         
+        p5.pop();
+      }
+    } else if (waveCheckbox.checked()) {
+      let wave;
+      p5.translate(window.innerWidth / 2, window.innerHeight / 2);
+
+      p5.translate(-(text.length-1)*fontSize.value()/2,0);
+
+      for(var i = 0; i < text.length; i++){
+        wave = p5.sin(p5.frameCount*speed.value() + i*waveLength.value()) * amplitude.value();
+        p5.fill(0);
+        
+        p5.push();
+        p5.translate(i*fontSize.value(), 0);
+        p5.text(text.charAt(i), 0, wave);
         p5.pop();
       }
     } else {
